0527684fb896671a93dfd8c145e014ba01395841
Mochamad Saepul M <numusai@gmail.com>
2023-06-28 07:06:45 +0700

    complete urshipper table

--- src/custom-data-table/custom-data-table.qore-component.tsx
+++ src/custom-data-table/custom-data-table.qore-component.tsx
@@ -6,58 +6,26 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
   Button,
   Portal,
+  InputGroup,
+  Input,
+  InputRightElement,
+  HStack,
+  IconButton,
 } from '@chakra-ui/react';
-
-const columns = [
-  {
-    name: 'action',
-    cell: (row: any) => (
-      <Menu>
-        <MenuButton color="gray.500" as={Button}>
-          ...
-        </MenuButton>
-        <Portal>
-          <MenuList style={{ zIndex: 1000 }}>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Delete</MenuItem>
-          </MenuList>
-        </Portal>
-      </Menu>
-    ),
-    allowOverflow: true,
-    button: true,
-  },
-
-  {
-    name: 'TitleX',
-    selector: (row: MyShipment) => row.title,
-    sortable: true,
-  },
-  {
-    name: 'Year',
-    selector: (row: MyShipment) => row.year,
-    sortable: true,
-  },
-  {
-    name: 'lookup_email_users',
-    selector: (row: MyShipment) => row.lookup_email_users,
-    sortable: true,
-  },
-];
+import { AddIcon, CloseIcon } from '@chakra-ui/icons';
 
 type MyShipment = {
   id: string;
   title: string;
   year: string;
   lookup_email_users: string;
+  status_form: string;
+  edit_action: () => void;
+  print_action: () => void;
+  create_invoice_action: () => void;
+  on_row_click_action: () => void;
 };
 
 const data: MyShipment[] = [];
@@ -68,46 +36,439 @@ const CustomDataTable = registerComponent('custom data table', {
   icon: 'TableAlt',
   group: 'data',
   display: 'block',
-  defaultProps: {},
-  propDefinition: {},
+  defaultProps: {
+    userId: '',
+    platformType: 'admin',
+    onInsertAction: [{ type: 'none' }],
+    onRowClickAction: [{ type: 'none' }],
+    onEditAction: [{ type: 'none' }],
+    printAction: [{ type: 'none' }],
+    createInvoiceAction: [{ type: 'none' }],
+  },
+  propDefinition: {
+    userId: { group: 'Text', type: 'expression', options: {} },
+    platformType: {
+      group: 'Text',
+      type: 'string',
+      options: {
+        format: 'select',
+        options: [
+          { label: 'Admin', value: 'admin' },
+          { label: 'Customer', value: 'client' },
+        ],
+      },
+    },
+    onInsertAction: {
+      group: 'On Insert',
+      label: 'On Insert',
+      type: 'action',
+      options: [{ type: 'none' }],
+    },
+    onRowClickAction: {
+      group: 'On Click',
+      label: 'Row On Click',
+      type: 'action',
+      options: [{ type: 'none' }],
+    },
+    onEditAction: {
+      group: 'On Edit',
+      label: 'On Edit',
+      type: 'action',
+      options: [{ type: 'none' }],
+    },
+    printAction: {
+      group: 'Print',
+      label: 'Print',
+      type: 'action',
+      options: [{ type: 'none' }],
+    },
+    createInvoiceAction: {
+      group: 'Create Invoice',
+      label: 'Create Invoice Action',
+      type: 'action',
+      options: [{ type: 'none' }],
+    },
+  },
+
   Component: (props: any) => {
     const source = props.source.target;
     const title = props.properties.title;
-    const userId = props.properties.user_id;
-    const platformType = props.properties.platform_type;
+    const userId = props.properties.userId;
+    const platformType = props.properties.platformType;
     const tableRowData = props.data.component?.rows;
+
+    const insertAction = props.hooks.useActionTrigger(
+      props.properties.onInsertAction,
+      props.data.page.row,
+      props.pageSource
+    );
+
+    const onEditAction = props.hooks.useActionTrigger(
+      props.properties.onEditAction,
+      props.data.page.row,
+      props.pageSource
+    );
+
+    const printAction = props.hooks.useActionTrigger(
+      props.properties.printAction,
+      props.data.page.row,
+      props.pageSource
+    );
+
+    const createInvoiceAction = props.hooks.useActionTrigger(
+      props.properties.createInvoiceAction,
+      props.data.page.row,
+      props.pageSource
+    );
+
+    const onRowClickAction = props.hooks.useActionTrigger(
+      props.properties.onRowClickAction,
+      props.data.page.row,
+      props.pageSource
+    );
 
     const [tableData, setTableData] = React.useState<MyShipment[]>(
       []
     );
-    // return JSON.stringify(tableRowData);
-    // insert tableRowData to data
 
     React.useEffect(() => {
       if (!tableRowData?.length || !source) return;
       setTableData(
         tableRowData.map((row: any) => ({
-          id: row.id,
-          title: row.sender_phone,
-          year: row.lookup_email_users,
-          lookup_email_users: row.lookup_email_users,
+          ...row,
+          edit_action: () => {
+            onEditAction.handleClick();
+          },
+          print_action: () => {
+            printAction.handleClick();
+          },
+          create_invoice_action: () => {
+            createInvoiceAction.handleClick();
+          },
+          on_row_click_action: () => {
+            onRowClickAction.handleClick();
+          },
         }))
       );
     }, [tableRowData, source]);
+
+    const [filterText, setFilterText] = React.useState('');
+    const [resetFilter, setResetFilter] =
+      React.useState<Boolean>(false);
+
+    const [selectedRow, setSelectedRow] = React.useState<
+      MyShipment[]
+    >([]);
+
+    const filteredItems = tableData.filter((item) => {
+      const filter = filterText.toLowerCase();
+      return Object.values(item).some(
+        (value) =>
+          value && value.toString().toLowerCase().includes(filter)
+      );
+    });
+
+    const subHeaderComponentMemo = React.useMemo(() => {
+      const handleClear = () => {
+        if (filterText) {
+          setResetFilter(!resetFilter);
+          setFilterText('');
+        }
+      };
+      return (
+        <HStack spacing={4}>
+          {platformType === 'client' && (
+            <IconButton
+              onClick={() => insertAction.handleClick()}
+              colorScheme="blue"
+              aria-label="Search database"
+              icon={<AddIcon />}
+            />
+          )}
+          <InputGroup size="md">
+            <Input
+              pr="3.5rem"
+              type="text"
+              value={filterText}
+              onChange={(e) => setFilterText(e.target.value)}
+              placeholder="Search"
+            />
+            <InputRightElement width="3.5rem">
+              {filterText && (
+                <Button h="1.75rem" size="sm" onClick={handleClear}>
+                  <CloseIcon />
+                </Button>
+              )}
+            </InputRightElement>
+          </InputGroup>
+        </HStack>
+      );
+    }, [filterText, resetFilter]);
+
+    /**
+     * TODO: need to confirm
+     * fixed action example (PRINT LABEL)
+     */
+    const actionPrintLabels = props.hooks.useActionTrigger(
+      //@ts-ignore
+      [
+        {
+          actionName: 'gcp_print_label',
+          notification: {
+            onSuccess: {
+              description: 'Your request successfully executed',
+              title: 'Success',
+            },
+            onFail: {
+              description: 'Oops, there is something wrong!',
+              title: 'Failed',
+            },
+          },
+          onFail: { type: 'none' },
+          payload: {
+            data: JSON.stringify(selectedRow.map((e) => e.id)),
+          },
+          showNotification: false,
+          type: 'trigger_action',
+        },
+      ],
+      props.data.page.row,
+      props.pageSource
+    );
+
+    /**
+     * TODO: need to confirm
+     * fixed action example (Create Invoice)
+     */
+    const actionCreateInvoices = props.hooks.useActionTrigger(
+      [
+        {
+          actionName: 'gcp_xendit_invoice',
+          notification: {
+            onSuccess: {
+              description: 'Your request successfully executed',
+              title: 'Success',
+            },
+            onFail: {
+              description: 'Oops, there is something wrong!',
+              title: 'Failed',
+            },
+          },
+          onFail: { type: 'none' },
+          payload: {
+            data: JSON.stringify(selectedRow.map((e) => e.id)),
+          },
+          showNotification: false,
+          type: 'trigger_action',
+        },
+        {
+          pageID: `6a3`,
+          rowID: `1`,
+          type: 'jump_to_page',
+          params: {
+            rowId: `1`,
+          },
+        },
+      ],
+      props.data.page.row,
+      props.pageSource
+    );
+
+    const ignoreColumns = [
+      'id',
+      'edit_action',
+      'print_action',
+      'create_invoice_action',
+      'on_row_click_action',
+    ];
+
+    const columns = [
+      {
+        name: 'ACTIONS',
+        cell: (row: MyShipment) => (
+          <Menu>
+            <MenuButton color="gray.500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="feather feather-more-vertical"
+                viewBox="0 0 24 24"
+              >
+                <circle cx="12" cy="12" r="1"></circle>
+                <circle cx="12" cy="5" r="1"></circle>
+                <circle cx="12" cy="19" r="1"></circle>
+              </svg>
+            </MenuButton>
+            <Portal>
+              <MenuList>
+                <MenuItem onClick={row.edit_action}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="feather feather-edit"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7"></path>
+                    <path d="M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                  </svg>
+                  &nbsp;Edit
+                </MenuItem>
+                <MenuItem onClick={row.print_action}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="feather feather-printer"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M6 9L6 2 18 2 18 9"></path>
+                    <path d="M6 18H4a2 2 0 01-2-2v-5a2 2 0 012-2h16a2 2 0 012 2v5a2 2 0 01-2 2h-2"></path>
+                    <path d="M6 14H18V22H6z"></path>
+                  </svg>
+                  &nbsp;Print Label
+                </MenuItem>
+                {platformType === 'admin' && (
+                  <MenuItem onClick={row.create_invoice_action}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="feather feather-clipboard"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M16 4h2a2 2 0 012 2v14a2 2 0 01-2 2H6a2 2 0 01-2-2V6a2 2 0 012-2h2"></path>
+                      <rect
+                        width="8"
+                        height="4"
+                        x="8"
+                        y="2"
+                        rx="1"
+                        ry="1"
+                      ></rect>
+                    </svg>
+                    &nbsp;Create Invoice
+                  </MenuItem>
+                )}
+              </MenuList>
+            </Portal>
+          </Menu>
+        ),
+        allowOverflow: true,
+        button: true,
+      },
+    ];
+
+    tableData[0]
+      ? Object.keys(tableData[0]).forEach((key) => {
+          if (!ignoreColumns.includes(key)) {
+            columns.push({
+              name: key.replace(/_/g, ' ').toUpperCase(),
+              //@ts-ignore
+              selector: (row: any) => row[key],
+              sortable: true,
+            });
+          }
+        })
+      : [];
 
     return (
       <div style={{ overflow: 'unset !important' }}>
         <DataTable
           columns={columns}
-          data={tableData}
+          data={filterText.length ? filteredItems : tableData}
           pagination
           selectableRows
           onSelectedRowsChange={(state) =>
-            console.log(state.selectedRows)
+            setSelectedRow(state.selectedRows)
           }
-          onRowClicked={(row) => console.log(row)}
+          subHeader
+          highlightOnHover
+          selectableRowDisabled={(row) =>
+            row.status_form === 'Completed'
+          }
+          subHeaderComponent={subHeaderComponentMemo}
+          onRowClicked={(row) => row.on_row_click_action()}
           onColumnOrderChange={(cols) => console.log(cols)}
         />
+        <br />
+
+        {selectedRow.length > 0 && (
+          <Menu>
+            <MenuButton as={Button} size="sm">
+              Action
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={actionPrintLabels}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="feather feather-printer"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M6 9L6 2 18 2 18 9"></path>
+                  <path d="M6 18H4a2 2 0 01-2-2v-5a2 2 0 012-2h16a2 2 0 012 2v5a2 2 0 01-2 2h-2"></path>
+                  <path d="M6 14H18V22H6z"></path>
+                </svg>
+                &nbsp;Print Label
+              </MenuItem>
+              {platformType === 'admin' && (
+                <MenuItem onClick={actionCreateInvoices}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="feather feather-clipboard"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M16 4h2a2 2 0 012 2v14a2 2 0 01-2 2H6a2 2 0 01-2-2V6a2 2 0 012-2h2"></path>
+                    <rect
+                      width="8"
+                      height="4"
+                      x="8"
+                      y="2"
+                      rx="1"
+                      ry="1"
+                    ></rect>
+                  </svg>
+                  &nbsp;Create Invoice
+                </MenuItem>
+              )}
+            </MenuList>
+          </Menu>
+        )}
       </div>
     );
   },
